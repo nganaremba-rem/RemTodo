f09bb2774b7530fa2ddab0281f65337324160698
remku <nganaremba@gmail.com>
2023-01-06 22:10:18 +0530

    Latest update

---
+++ app/assets/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  darkBg: "#313541",
+  orange: "rgb(250,114,95)",
+  lightGray: "rgb(63,70,83)",
+  green: "rgb(178,210,153)",
+  purple: "rgb(171,153,252)",
+  yellow: "rgb(249,171,77)",
+};
